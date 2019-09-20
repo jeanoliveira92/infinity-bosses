241e7085decb581a65ad4a45e7926e00526a8d47
Jean Carlos de Oliveira <jeancarlosdeoliveira@outlook.com>
2019-09-20 02:48:57 -0300

    Intro
    Intro

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -7,6 +7,7 @@ public class UIController : MonoBehaviour
 {
 
     public AudioSource backgroundSound; 
+    public GameObject IntroUI;
     public GameObject MainCanvas;
     public GameObject characterCanvas;
     public GameObject shadowUI;
@@ -15,11 +16,34 @@ public class UIController : MonoBehaviour
     private GameObject activeUI;
     private int charSelected = 1;
 
+    IEnumerator WaitAndMainMenu()
+    {
+        yield return new WaitForSeconds(3.0f);
+        backgroundSound.Play();
+        GameObject parent = IntroUI.transform.parent.gameObject;
+        MainCanvas.SetActive(true);
+        parent.SetActive(false); 
+    }
+    IEnumerator WaitAndIntroUnifei()
+    {
+        yield return new WaitForSeconds(3.0f);
+        GameObject unifei = IntroUI.transform.GetChild(2).gameObject;
+        unifei.SetActive(true);  
+        StartCoroutine("WaitAndMainMenu");
+    }
+    IEnumerator WaitAndIntroUnity()
+    {
+        yield return new WaitForSeconds(2f);
+        GameObject unity = IntroUI.transform.GetChild(1).gameObject;
+        unity.SetActive(true);  
+        StartCoroutine("WaitAndIntroUnifei");
+
+    }
     void Start()
     {
+        StartCoroutine("WaitAndIntroUnity");
         backgroundSound = GetComponent<AudioSource>();
-        backgroundSound.Play();
-        activeUI = shadowUI;
+        //backgroundSound.Play();
       
     }
 
@@ -33,6 +57,7 @@ public class UIController : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
         characterCanvas.SetActive(true);
         MainCanvas.SetActive(false);
+        activeUI = shadowUI;
     }
     public void Play(GameObject obj){
         StartCoroutine("WaitAndPLay");
