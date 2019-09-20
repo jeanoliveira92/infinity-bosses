ae5cba25297f3c9fe334d9f8f4d2b855e806b47b
Jean Carlos de Oliveira <jeancarlosdeoliveira@outlook.com>
2019-09-20 01:51:20 -0300

    FullMenu
    Menu inicial e menu de seleção de personagens

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -6,8 +6,20 @@ using UnityEngine.SceneManagement;
 public class UIController : MonoBehaviour
 {
 
+    public AudioSource backgroundSound; 
+    public GameObject MainCanvas;
+    public GameObject characterCanvas;
+    public GameObject shadowUI;
+    public GameObject sephirothUI;
+    public GameObject thanosUI;
+    private GameObject activeUI;
+    private int charSelected = 1;
+
     void Start()
     {
+        backgroundSound = GetComponent<AudioSource>();
+        backgroundSound.Play();
+        activeUI = shadowUI;
       
     }
 
@@ -16,9 +28,68 @@ public class UIController : MonoBehaviour
     {
 
     }
+    IEnumerator WaitAndPLay()
+    {
+        yield return new WaitForSeconds(0.5f);
+        characterCanvas.SetActive(true);
+        MainCanvas.SetActive(false);
+    }
+    public void Play(GameObject obj){
+        StartCoroutine("WaitAndPLay");
+    }
 
-    public void Play(){
+    public void SelectCharacter(int id){
+        
+        charSelected = id;
+
+        if(id == 1){
+            shadowUI.SetActive(true);
+            thanosUI.SetActive(false);
+            sephirothUI.SetActive(false);
+            activeUI = shadowUI;
+        }else if(id == 2){
+            thanosUI.SetActive(true);
+            sephirothUI.SetActive(false);
+            shadowUI.SetActive(false);
+            activeUI = thanosUI;
+        }else if(id == 3){
+            sephirothUI.SetActive(true);
+            thanosUI.SetActive(false);
+            shadowUI.SetActive(false);
+            activeUI = sephirothUI;
+        }
+
+    }
+    IEnumerator WaitToEnterScene()
+    {
+        yield return new WaitForSeconds(8.8f);
         SceneManager.LoadScene(1);
-       
+    }
+    IEnumerator WaitToShowLogo()
+    {
+        yield return new WaitForSeconds(3.0f);
+        
+        GameObject logo = activeUI.transform.GetChild(5).gameObject;
+        logo.SetActive(true);       
+    }
+    IEnumerator WaitToEnterAnimationScene()
+    {
+        yield return new WaitForSeconds(3.0f);
+        StartCoroutine("WaitToShowLogo");
+        
+        GameObject img = activeUI.transform.GetChild(4).gameObject;
+        img.SetActive(true);  
+    }
+    public void StageSelect(){
+        StartCoroutine("WaitToEnterScene");
+        StartCoroutine("WaitToEnterAnimationScene");
+        
+        GameObject bg = activeUI.transform.GetChild(1).gameObject;
+        GameObject label = activeUI.transform.GetChild(2).gameObject;
+        GameObject texto = activeUI.transform.GetChild(3).gameObject;
+
+        bg.SetActive(false);
+        label.SetActive(false);
+        texto.SetActive(false);
     }
 }
