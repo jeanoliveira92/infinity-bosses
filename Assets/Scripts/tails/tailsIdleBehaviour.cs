c17a06e6ff14b868126452b9e8ea43cdb2a4eadd
Wesley <51175134+wesleyaag@users.noreply.github.com>
2019-11-26 19:27:52 -0300

    Tails ajustes (não terminado)
    Ainda tá bugadaço
    -não pula direito
    -não atira direito

---
+++ Assets/Scripts/tails/tailsIdleBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tailsIdleBehaviour : StateMachineBehaviour
+{
+    private int sort;
+    public float timer;
+    public float minTime;
+    public float maxTime;
+    
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       sort = Random.Range(1, 3);
+       timer = Random.Range(minTime,maxTime);
+        switch (sort)
+        {
+            case 1:
+                animator.SetTrigger("Jump");
+                break;
+
+            case 2:
+                animator.SetTrigger("shooting");
+                break;
+        }
+    }
+
+    
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(timer <= 0){
+            switch (sort)
+            {
+            case 1:
+                animator.SetTrigger("Jump");
+                break;
+
+            case 2:
+                animator.SetTrigger("shooting");
+                break;
+        }
+        }
+        else {
+            timer -= Time.deltaTime;
+        }
+    }
+
+    
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+}
